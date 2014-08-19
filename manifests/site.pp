2080d9863836f82fbaa6800fe82c93f9c49744ff
Adam Clarke <adamclarkewastaken@gmail.com>
2014-08-19 11:48:26 -0500

    add ruby version 2.1.2 for rbenv

--- manifests/site.pp
+++ manifests/site.pp
@@ -86,6 +86,7 @@ node default {
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
+  ruby::version { '2.1.2': }
 
   class { 'ruby::global' :
     version => '2.0.0'
