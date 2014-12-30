23465be7f68402e5b1a457f1a2003a6434c1e0c0
Adam Clarke <adamclarkewastaken@gmail.com>
2014-12-30 10:52:48 -0600

    Update weblinc_dummy_app.pp

--- modules/projects/manifests/weblinc_dummy_app.pp
+++ modules/projects/manifests/weblinc_dummy_app.pp
@@ -2,7 +2,6 @@ class projects::weblinc_dummy_app {
 
   boxen::project { 'weblinc-dummy-app':
     ruby => '2.0.0-p353',
-    dotenv => true,
     nginx => true,
     elasticsearch => true,
     source => 'git@git.tools.weblinc.com:weblinc-dummy-app.git'
