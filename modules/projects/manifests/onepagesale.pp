3a28cd6b149daa4b2cff505a08b9e1a30e73b096
Adam Clarke <adamclarkewastaken@gmail.com>
2014-12-30 10:52:14 -0600

    Update onepagesale.pp

--- modules/projects/manifests/onepagesale.pp
+++ modules/projects/manifests/onepagesale.pp
@@ -2,7 +2,6 @@ class projects::onepagesale {
 
   boxen::project { 'onepagesale':
     ruby => '2.0.0',
-    dotenv => true,
     postgresql => true,
     nginx => true,
     source => 'revelrylabs/onepagesale'
