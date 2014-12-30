885c1b023574353c6cc9681b4a3802412e8edf65
Adam Clarke <adamclarkewastaken@gmail.com>
2014-12-30 10:52:04 -0600

    Update mobstreams.pp

--- modules/projects/manifests/mobstreams.pp
+++ modules/projects/manifests/mobstreams.pp
@@ -2,7 +2,6 @@ class projects::mobstreams {
 
   boxen::project { 'mobstreams':
     ruby   => '2.0.0',
-    dotenv => true,
     postgresql  => true,
     nginx  => true,
     source => 'revelrylabs/mobstreams'
