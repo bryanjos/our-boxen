d94ae05d1fc07349de6d4460d1cb1489a9e4df76
Laura Hill <iluvcrayons@gmail.com>
2014-12-30 16:20:01 -0600

    idunnolol

--- manifests/site.pp
+++ manifests/site.pp
@@ -75,9 +75,9 @@ node default {
   include screenhero
 
   # fail if FDE is not enabled
-  if $::root_encrypted == 'no' {
-    fail('Please enable full disk encryption and try again')
-  }
+  # if $::root_encrypted == 'no' {
+  #  fail('Please enable full disk encryption and try again')
+  # }
 
   # node versions
   nodejs::version { 'v0.6': }
