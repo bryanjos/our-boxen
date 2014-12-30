d9fc1c948dc8a29e52623ae09cfae1903c74720d
Adam Clarke <adamclarkewastaken@gmail.com>
2014-12-30 10:59:51 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -80,9 +80,9 @@ node default {
   }
 
   # node versions
-  # include nodejs::v0_6 # 0.6 is removed?
-  include nodejs::v0_8
-  include nodejs::v0_10
+  nodejs::version { 'v0.6': }
+  nodejs::version { 'v0.8': }
+  nodejs::version { 'v0.10': }
 
   # default ruby versions
   ruby::version { '1.9.3': }
