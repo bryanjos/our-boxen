6b9fb1392df37078b2517c7bcce65e46be97e4d9
Adam Clarke <adamclarkewastaken@gmail.com>
2015-12-07 15:50:40 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -95,6 +95,8 @@ node default {
   ruby::version { '2.1.7': }
   ruby::version { '2.2.2': } 
   ruby::version { '2.2.3': }
+  
+  class { 'ruby::global': version => '2.2.2' }
 
   # common, useful packages
   package {
